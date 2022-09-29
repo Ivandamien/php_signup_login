067d408379f6bea69d0a25137fcbed8ad1ae4761
Ivan Okello <ivandamieno99@gmail.com>
2022-09-29 03:52:07 +0300

    Initial php signup-login form commit

---
+++ login.php
@@ -0,0 +1,71 @@
+<?php
+
+$is_invalid =false;
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+   
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = sprintf("SELECT * FROM user
+            WHERE email = '%s'",
+            $mysqli->real_escape_string($_POST["email"]));
+
+   $result = $mysqli->query($sql);  
+   $user = $result->fetch_assoc();   
+//    var_dump($user);
+//    exit;    
+
+    if($user){
+
+       if (password_verify($_POST["password"], $user["password_hash"])){
+            // die("Login successful");
+
+            session_start();
+
+            session_regenerate_id();
+
+            $_SESSION["user_id"] = $user["id"];   
+            header("Location: index.php");
+            exit;
+        }
+    }
+     
+$is_invalid = true;
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+
+    <?php if ($is_invalid): ?>
+    <em>Invalid Login</em>
+
+    <?php endif; ?>
+
+    <form action="" method="post">
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email"
+                value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <button>Login</button>
+    </form>
+  
+</body>
+</html>
